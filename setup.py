abb97d12ee5081d2ba7d16c0a29afdccb4e19ac2
Michał Górny <mgorny@gentoo.org>
2017-11-28 09:53:13 +0100

    Switch to setuptools

--- setup.py
+++ setup.py
@@ -3,18 +3,22 @@
 # Copyright (c) 2017 Gentoo Foundation
 # Placed in public domain
 
-from distutils.core import setup
+from setuptools import find_packages, setup
 
 setup(
     name='docutils_glep',
     version='1.0',
+    description='docutils modules & wrapper to process Gentoo Linux Extension Proposals',
+
     author='Gentoo Foundation',
     author_email='glep@gentoo.org',
     url='http://github.com/gentoo/docutils-glep',
 
-    packages=['docutils_glep', 'docutils_glep.html_writer'],
+    packages=find_packages(),
     package_data={'docutils_glep.html_writer': ['*.css', '*.txt']},
     scripts=['glep'],
+
+    install_requires=['docutils'],
 
     classifiers=[
         'Development Status :: 5 - Production/Stable',
