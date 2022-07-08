322bc266f2724da2d725177d3fd0cf09bf5eba20
Michał Górny <mgorny@gentoo.org>
2022-07-08 17:10:11 +0200

    reader: Transform yaml preamble into rfc2822 format
    Signed-off-by: Michał Górny <mgorny@gentoo.org>

--- docutils_glep/reader.py
+++ docutils_glep/reader.py
@@ -1,4 +1,5 @@
 # docutils GLEP support
+# Copyright (c) 2022 Michał Górny
 # Copyright (c) 2017 Gentoo Foundation
 # Placed in public domain
 # based on PEP code by:
@@ -15,6 +16,9 @@ Gentoo Linux Enhancement Proposal (GLEP) Reader.
 
 __docformat__ = 'reStructuredText'
 
+from email.message import EmailMessage
+
+import yaml
 
 from docutils import DataError
 from docutils.parsers import rst
@@ -28,19 +32,18 @@ class PreambledRstParser(rst.Parser):
 
     def parse(self, inputstring, document):
         if inputstring.startswith('---\n'):
-            spl = inputstring.split('---\n', 2)
-            assert not spl[0]
+            _, yaml_text, glep_text = inputstring.split('---\n', 2)
+            assert not _
             try:
-                import yaml
-            except ImportError:
-                pass
-            else:
-                try:
-                    yaml.safe_load(spl[1])
-                except Exception as e:
-                    raise DataError('Header preamble is not valid YAML:\n%s' % e)
-            # remove ---s to let regular parser handle it
-            inputstring = ''.join(spl)
+                yaml_data = yaml.safe_load(yaml_text)
+            except Exception as e:
+                raise DataError('Header preamble is not valid YAML:\n%s' % e)
+            rfc_header = EmailMessage()
+            for k, v in yaml_data.items():
+                if v is None:
+                    v = ""
+                rfc_header[k] = str(v)
+            inputstring = str(rfc_header) + glep_text
 
         super(PreambledRstParser, self).parse(inputstring, document)
 
